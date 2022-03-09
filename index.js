bfd86d78bec3c5d4c882cc0830104e2a4e0b75a0
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-03-08 18:59:12 -0800

    Completed the first five tasks

--- index.js
+++ index.js
@@ -223,11 +223,11 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(kilometers){
+  return kilometers * 0.621371;
 }
 
-
+console.log(miles(10));
 
 //Task 5b - Feet to CM
 /*
@@ -237,11 +237,11 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  return cm / 30.48;
 }
 
-
+console.log(feet(35));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
